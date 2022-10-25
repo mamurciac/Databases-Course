47bc8615be38ee863c10e4105723103dbef30da7
Michael Andrés Murcia <mamurciac@unal.edu.co>
2022-10-25 14:18:20 -0500

    .

--- Script 1 Manejo de Consultas.sql
+++ Script 1 Manejo de Consultas.sql
@@ -10,7 +10,17 @@ show tables;
 #Se muestra la estructura y descripcion de los campos de una tabla dada (En este caso, de la tabla actor, la cual esta almacenada en la base de datos sakila)
 describe actor;
 
-#Se 
+#Se realiza una consulta en la cual se muestra toda la informacion de tabla actor
 select * from actor;
 
+#Se realiza una consulta en la cual se muestra toda la informacion de tabla actor pero proyectando solo la columna del primer nombre
 select first_name from actor;
+
+#Se crea una nueva base de datos (Esto se hace si no existe la base de datos db1)
+create schema if not exists db1;
+
+#Se selecciona una base de datos para trabajar sobre ella (En MySQL se conocen como schemas)
+use db1;
+
+#Se elimina una base de datos (Esto se hace si existe la base de datos db1)
+drop schema if exists db1;
